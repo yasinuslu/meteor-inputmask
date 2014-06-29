2c66b7328aa035181682cbd116d3c699f93fc04c
Yasin Uslu <nepjua@gmail.com>
2014-06-29 17:31:45 +0300

    initial package

---
+++ package.js
@@ -0,0 +1,18 @@
+// Give our package a description
+Package.describe({
+  summary: 'jQuery inputmask plugin'
+});
+
+// Tell Meteor what to do with our package at bundle time
+Package.on_use(function(api) {
+
+  // The api.use method allows us to depend on other
+  // packages that ship with meteor or are in our project's
+  // package directory
+  api.use('jquery', 'client');
+
+
+  // Add templates.html and client.js files ONLY on
+  // the client
+  api.add_files('lib/jquery.inputmask.bundle.js', 'client');
+});
